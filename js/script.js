3d9711a7f4362b6ef8b58fd4e7a404a01ccd776c
Samuel Eli Heletsi <samuelheletsi@gmail.com>
2024-07-15 13:47:46 +0000

    sucre1

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// ========= Typing animation ============
+var typed = new Typed(".typing", {
+    strings:["Software Engineer", "Network Engineer","Graphic Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})    
